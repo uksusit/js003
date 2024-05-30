3fbd25823bac610ec525dd22b9431b0f8aac07d5
uksusit <140089654+uksusit@users.noreply.github.com>
2024-05-30 20:18:56 +0300

    3.3.9

---
+++ js/a3.3.8.js
@@ -0,0 +1,23 @@
+// Доповни код оголошення об'єкта таким чином, щоб у нього були властивості name, price, image і tags зі значеннями зі змінних з аналогічними іменами. Обов'язково використовуй синтаксис коротких властивостей.
+
+// Оголошена змінна product
+// Значення змінної product - це об'єкт
+// Значення вкладеної властивості name - це рядок "Repair Droid"
+// Значення вкладеної властивості price - це число 2500
+// Значення вкладеної властивості image - це рядок "https://via.placeholder.com/640x480"
+// Значення вкладеної властивості tags - це масив["on sale", "trending", "best buy"]
+
+
+const name = "Repair Droid";
+const price = 2500;
+const image = "https://via.placeholder.com/640x480";
+const tags = ["on sale", "trending", "best buy"];
+
+const product = {
+  // Change code below this line
+  name,
+  price,
+  image,
+  tags,
+  // Change code above this line
+};
---
+++ js/a3.3.9.js
@@ -0,0 +1,22 @@
+// Доповни код оголошення об'єкта credentials таким чином, щоб в результаті у нього були дві властивості: email і password, імена яких зберігаються у змінних emailInputName і passwordInputName.
+
+// Значенням властивості email повинен бути рядок "henry.carter@aptmail.com", а значенням властивості password - рядок "jqueryismyjam".
+
+// Оголошена змінна credentials
+// Значення змінної credentials - це об'єкт
+// Об'єкт credentials містить властивість email
+// Значення вкладеної властивості email - це рядок "henry.carter@aptmail.com"
+// Об'єкт credentials містить властивість password
+// Значення вкладеної властивості password - це рядок "jqueryismyjam"
+
+const emailInputName = "email";
+const passwordInputName = "password";
+
+const credentials = {
+  // Change code below this line
+[emailInputName]: "henry.carter@aptmail.com",
+[passwordInputName]: "jqueryismyjam",
+
+
+  // Change code above this line
+};
