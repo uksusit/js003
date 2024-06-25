7775f6b59323f64557fec7377851a359860d74aa
uksusit <140089654+uksusit@users.noreply.github.com>
2024-06-25 09:28:10 +0300

    a3.3.11-12

--- js/a3.3.10.js
+++ js/a3.3.10.js
@@ -1,25 +1,59 @@
-// На відміну від масиву або рядка, об'єкт - це не ітерабельна сутність, тобто його не можна перебрати циклами for або for...of. Для перебирання об'єктів використовується спеціальний цикл for...in, який перебирає ключі об'єкта object.
+// На відмінузапиши в масив keys всі його ключі від масиву або рядка, об'єкт - це не ітерабельна сутність, тобто його не можна перебрати циклами for або for...of. Для перебирання об'єктів використовується спеціальний цикл for...in, який перебирає ключі об'єкта object.
 
 // for (key in object) {
 //   // інструкції
 // }
 // Змінна key доступна тільки в тілі циклу. На кожній ітерації в неї буде записано значення ключа (ім'я) властивості. Для того щоб отримати значення властивості з таким ключем (ім'ям), використовується синтаксис квадратних дужок.
 
-const book = {
-  title: "The Last Kingdom",
-  author: "Bernard Cornwell",
-  genres: ["historical prose", "adventure"],
-  rating: 8.38,
-};
+// const book = {
+//   title: "The Last Kingdom",
+//   author: "Bernard Cornwell",
+//   genres: ["historical prose", "adventure"],
+//   rating: 8.38,
+// };
 
-for (const key in book) {
-  // Ключ
-  console.log(key);
-  // Значення властивості з таким ключем
-  console.log(book[key]);
-}
+// for (const key in book) {
+//   // Ключ
+//   console.log(key);
+//   // Значення властивості з таким ключем
+//   console.log(book[key]);
+// }
 
 //   --------------------------------------------
+
+// Перебери об'єкт apartment, використовуючи цикл for...in,
+//  і , а в масив values 
+//  всі значення його властивостей.
+
+
+// Оголошена змінна apartment
+// Значення змінної apartment - це об'єкт
+// Оголошена змінна keys
+// Значення змінної keys - це масив ["descr", "rating", "price"]
+// Оголошена змінна values
+// Значення змінної values - це масив ["Spacious apartment in the city center", 4, 2153]
+
+
+// const apartment = {
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+// };
+// const keys = [];
+// const values = [];
+// // Change code below this line
+
+// for (const key in apartment) {
+//   push.keys = key;
+// //   push.values = key;
+//   console.log(key);
+//   console.log(...key);
+  
+// }
+
+// console.log(...keys);
+// console.log(...values);
+
 
 
 const apartment = {
@@ -31,13 +65,16 @@ const keys = [];
 const values = [];
 // Change code below this line
 
+
 for (const key in apartment) {
-  push.keys = key;
-//   push.values = key;
-  console.log(key);
-  console.log(...key);
-  
+  // Ключ
+  console.log("key=" , key);
+  keys.push(key);
+  // Значення властивості з таким ключем
+  console.log("key value=", apartment[key]);
+  values.push(apartment[key]);
 }
 
-console.log(...keys);
-console.log(...values);
+
+console.log(keys);
+console.log(values);
---
+++ js/a3.3.11.js
@@ -0,0 +1,110 @@
+
+// Розберемо концепцію власних і невласних властивостей об'єкта
+//  і навчимося правильно використовувати цикл for...in.
+
+// const animal = {
+//   legs: 4,
+// };
+// const dog = Object.create(animal);
+// dog.name = "Mango";
+
+// console.log(dog); // {name: 'Mango'}
+// console.log(dog.name); // 'Mango'
+// console.log(dog.legs); // 4
+// Метод Object.create(animal) створює і повертає новий об'єкт, зв'язуючи його з об'єктом animal. Тому можна отримати значення властивості legs, звернувшись до неї як dog.legs, хоча вона відсутня в об'єкті dog - це невласна властивість з об'єкта animal.
+
+// Оператор in, який використовується в циклі for...in, не розрізняє власні та невласні властивості об'єкта. Ця особливість заважає, оскільки ми завжди хочемо перебрати тільки власні властивості. Для того щоб дізнатися, чи є в об'єкті власна властивість, чи немає, використовується метод hasOwnProperty(key), який повертає true або false.
+
+// // ❌ Повертає true для всіх властивостей
+// console.log("name" in dog); // true
+// console.log("legs" in dog); // true
+
+// // ✅ Повертає true тільки для власних властивостей
+// console.log(dog.hasOwnProperty("name")); // true
+// console.log(dog.hasOwnProperty("legs")); // false
+// Тому під час перебору циклом for...in необхідно на кожній ітерації додати перевірку на власну властивість. Навіть якщо зараз ми впевнені у тому, що об'єкт не містить невласні властивості, це захистить від можливих помилок в майбутньому.
+
+// const book = {
+//   title: "The Last Kingdom",
+//   author: "Bernard Cornwell",
+//   genres: ["historical prose", "adventure"],
+//   rating: 8.38,
+// };
+
+// for (const key in book) {
+//   // Якщо це власна властивість - виконуємо тіло if
+//   if (book.hasOwnProperty(key)) {
+//     console.log(key);
+//     console.log(book[key]);
+//   }
+
+//   // Якщо це невласна властивість - нічого не робимо
+// }
+
+// Виконай рефакторинг рішення попереднього завдання,
+//  додавши в цикл for...in перевірку на власну властивість.
+
+// Оголошена змінна advert.
+// Значення змінної advert - це об'єкт.
+// Оголошена змінна apartment.
+// Значення змінної apartment - це об'єкт.
+// Оголошена змінна keys.
+// Значення змінної keys - це масив ["descr", "rating", "price"].
+// Оголошена змінна values.
+// Значення змінної values - це масив ["Spacious apartment in the city center", 4, 2153].
+
+
+
+const keys = [];
+const values = [];
+const advert = {
+  service: "apt",
+};
+const apartment = Object.create(advert);
+console.log(advert);
+apartment.descr = "Spacious apartment in the city center";
+apartment.rating = 4;
+apartment.price = 2153;
+
+for (const key in apartment) {
+    // Change code below this line
+    if (apartment.hasOwnProperty(key)) {
+        keys.push(key);
+        values.push(apartment[key]);
+
+        // Change code above this line
+    }
+}
+
+console.log(keys);
+console.log(values);
+console.log(advert);
+
+
+
+//   -------  12 ---------------------
+
+// Напиши функцію countProps(object), яка рахує і повертає кількість власних властивостей об'єкта в параметрі object. Використовуй змінну propCount для зберігання кількості властивостей об'єкта.
+
+// Оголошена функція countProps(object)
+// Виклик countProps({}) повертає 0
+// Виклик countProps({ name: "Mango", age: 2 }) повертає 2
+// Виклик countProps({ mail: "poly@mail.com", isOnline: true, score: 500 }) повертає 3
+// Функція підраховує тільки власні властивості об'єкта
+
+
+
+function countProps(object) {
+  let propCount = 0;
+  // Change code below this line
+for ( const obj in object){
+  if (object.hasOwnProperty(obj)){
+    
+  propCount += 1;
+  }
+  
+}
+  
+  // Change code above this line
+  return propCount;
+}
