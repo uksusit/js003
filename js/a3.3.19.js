57e8c5b0c03b4a1cbc9be3dc3ed853a77efa3b03
uksusit <140089654+uksusit@users.noreply.github.com>
2024-07-04 18:53:07 +0300

    19

---
+++ js/a3.3.19.js
@@ -0,0 +1,33 @@
+const products = [
+  { name: "Radar", price: 1300, quantity: 4 },
+  { name: "Scanner", price: 2700, quantity: 3 },
+  { name: "Droid", price: 400, quantity: 7 },
+  { name: "Grip", price: 1200, quantity: 9 },
+];
+
+function getAllPropValues(propName) {
+  // Change code below this line
+  
+const prodts =[];
+    for (const product of products) {
+        console.log("product: ", product);
+        
+        const { name, price, quantity } = product;
+
+        const values = Object.values(product);
+        console.log("values: ", values);
+        
+        if (product.key = propName) {
+            console.log("product.key: ", product.key);
+            
+            console.log("product.value: ", product.value);
+
+            prodts.push(product.value);
+            console.log("return: ", prodts);
+        }
+    }
+
+  // Change code above this line
+}
+
+getAllPropValues("name")
