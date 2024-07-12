cc81556dd6a55a382426aeb565c741d1b6c2228c
uksusit <140089654+uksusit@users.noreply.github.com>
2024-07-12 13:10:23 +0300

    a27

--- js/a3.3.27.js
+++ js/a3.3.27.js
@@ -1,2 +1,10 @@
 
 
+const scores = [89, 64, 42, 17, 93, 51, 26];
+// Change code below this line
+
+const bestScore = (Math.max(... scores));
+console.log(bestScore);
+const worstScore = (Math.min(... scores));
+console.log(worstScore);
+
