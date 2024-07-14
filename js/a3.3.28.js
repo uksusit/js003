01ebb29c1fc3e4abbf5b4bf3c3b3854fc1ba8701
uksusit <140089654+uksusit@users.noreply.github.com>
2024-07-14 13:17:15 +0300

    a28

--- js/a3.3.28.js
+++ js/a3.3.28.js
@@ -2,15 +2,22 @@ const firstGroupScores = [64, 42, 93];
 const secondGroupScores = [89, 14, 51, 26];
 const thirdGroupScores = [29, 47, 18, 97, 81];
 // Change code below this line
-const allScores = [];
-const bestScore = allScores;
-const worstScore = allScores;
+
+const allScores = [...firstGroupScores, 
+                   ...secondGroupScores, 
+                   ...thirdGroupScores
+                ];
+const bestScore = (Math.max(...allScores));
+const worstScore = (Math.min(...allScores));
+
+console.log(allScores, bestScore, worstScore);
 
 
 
+// У змінних firstGroupScores, secondGroupScores і thirdGroupScores 
+// зберігаються результати тестування окремих груп.Використовуючи розподіл,
+//  доповни код таким чином, щоб:
 
-
-// У змінних firstGroupScores, secondGroupScores і thirdGroupScores зберігаються результати тестування окремих груп. Використовуючи розподіл, доповни код таким чином, щоб:
 
 // У змінній allScores зберігався масив всіх результатів від першої до третьої групи.
 // У змінній bestScore був найвищий загальний бал.
