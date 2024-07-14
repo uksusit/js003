2dc21442313a22d1e534a19a386600bde4c144ed
uksusit <140089654+uksusit@users.noreply.github.com>
2024-07-14 13:49:24 +0300

    a29

---
+++ js/a3.3.29.js
@@ -0,0 +1,36 @@
+
+// В конструкторі можна створювати нові тести, для яких є налаштування за замовчуванням,
+//  які зберігаються у змінній defaultSettings.Під час створення тесту, усі або частину 
+// налаштувань можна перевизначити, вони зберігаються у змінній overrideSettings.
+
+// Для того щоб отримати фінальні налаштування тесту, необхідно взяти налаштування
+//  за замовчуванням і поверх них застосувати перевизначені налаштування.Доповни код 
+// таким чином, щоб у змінній finalSettings утворився об'єкт фінальних налаштувань тесту.
+
+// Оголошена змінна defaultSettings
+// Значення змінної defaultSettings - це об'єкт
+// Оголошена змінна overrideSettings
+// Значення змінної overrideSettings - це об'єкт
+// Оголошена змінна finalSettings
+// Значення змінної finalSettings - це об'єкт
+// Значення властивості finalSettings.theme дорівнює "light"
+// Значення властивості finalSettings.public дорівнює "false"
+// Значення властивості finalSettings.withPassword дорівнює "true"
+// Значення властивості finalSettings.minNumberOfQuestions дорівнює 10
+// Значення властивості finalSettings.timePerQuestion дорівнює 30
+// Для присвоєння значення змінній finalSettings використовується синтаксис ...
+
+const defaultSettings = {
+  theme: "light",
+  public: true,
+  withPassword: false,
+  minNumberOfQuestions: 10,
+  timePerQuestion: 60,
+};
+const overrideSettings = {
+  public: false,
+  withPassword: true,
+  timePerQuestion: 30,
+};
+// Change code below this line
+const finalSettings = {...defaultSettings, ...overrideSettings};
