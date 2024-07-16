b1c9109f7a5058a16da3fe2cf3d614930a0b487d
uksusit <140089654+uksusit@users.noreply.github.com>
2024-07-16 08:40:34 +0300

    a31

---
+++ js/a3.3.31.js
@@ -0,0 +1,14 @@
+// Change code below this line
+function add(...args) {
+    let sum = 0;
+
+    for (let arg of args) sum += arg;
+    console.log(sum);
+
+  return sum;
+  
+
+  
+  // Change code above this line
+}
+add(74, 11, 62, 46, 12, 36);
