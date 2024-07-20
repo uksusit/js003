5cf2c508d04fdc9c487ae7655fceadba6b7540d5
uksusit <140089654+uksusit@users.noreply.github.com>
2024-07-20 17:16:37 +0300

    a33ok

---
+++ js/a3.3.34.js
@@ -0,0 +1,26 @@
+// Розшир функціональні можливості об’єкта bookShelf, додавши два нові методи. Ці методи мають працювати подібно до існуючих методів getBooks() і addBook(bookName), але з іншими цілями.
+
+// Метод removeBook(bookName) має повертати рядок у форматі "Deleting book <назва книги>", де <назва книги> — це значення параметра bookName.
+
+// Метод updateBook(oldName, newName) має повертати рядок у форматі "Updating book <стара назва> to <нова назва>", де <стара назва> і <нова назва> є значеннями параметрів oldName і newName відповідно.
+
+// Оголошена змінна bookShelf
+
+// Значення змінної bookShelf - це об'єкт
+
+// Значення властивості bookShelf.getBooks - це метод об'єкта
+
+// Виклик методу bookShelf.getBooks() повертає рядок "Returning all books"
+
+// Значення властивості bookShelf.addBook - це метод об'єкта
+
+// Виклик методу bookShelf.addBook("Haze") повертає рядок "Adding book Haze"
+
+// Значення властивості bookShelf.removeBook - це метод об'єкта
+
+// Виклик методу bookShelf.removeBook("Red sunset") повертає рядок "Deleting book Red sunset"
+
+// Значення властивості bookShelf.updateBook - це метод об'єкта
+
+// Виклик методу bookShelf.updateBook("Sands of dune", "Dune") повертає рядок "Updating book Sands of dune to Dune"
+
