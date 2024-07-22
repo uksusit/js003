701e3d13e39040719e05ad27e383775a46435b83
uksusit <140089654+uksusit@users.noreply.github.com>
2024-07-22 18:44:37 +0300

    36

---
+++ js/a3.3.36.js
@@ -0,0 +1,7 @@
+const atTheOldToad = {
+  potions: ["Speed potion", "Dragon breath", "Stone skin"],
+  // Change code below this line
+getPotions() {return {potions}
+  // Change code above this line
+};
+atTheOldToad.getPotions()
