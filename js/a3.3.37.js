4ca25999525d7b6391f85975c193008447671d37
uksusit <140089654+uksusit@users.noreply.github.com>
2024-07-23 19:48:52 +0300

    40

--- js/a3.3.37.js
+++ js/a3.3.37.js
@@ -12,18 +12,42 @@
 
 // ------------39-----------
 
+// const atTheOldToad = {
+//   potions: ["Speed potion", "Dragon breath", "Stone skin"],
+//   removePotion(potionName) {
+//     // Change code below this line
+//         const values = Object.values(this.potions);
+//       console.log(values);
+//       console.log(this.potions.indexOf(potionName));
+//       this.potions.splice(this.potions.indexOf(potionName), 1);
+//       console.log(this.potions);
+      
+//       return potionName;
+//     // Change code above this line
+//   },
+// };
+// atTheOldToad.removePotion('Dragon breath')
+
+// --------40-------------------
+
+
 const atTheOldToad = {
   potions: ["Speed potion", "Dragon breath", "Stone skin"],
-  removePotion(potionName) {
+  updatePotionName(oldName, newName) {
     // Change code below this line
-        const values = Object.values(this.potions);
-      console.log(values);
-      console.log(this.potions.indexOf(potionName));
-      this.potions.splice(this.potions.indexOf(potionName), 1);
+  console.log(this.potions); 
+    console.log(oldName, newName);
+    console.log(this.updatePotionName);
+     
+let index = this.potions.indexOf(oldName) ;
+    console.log(index); 
+
+  this.potions.splice(index, 1, newName);
       console.log(this.potions);
-      
-      return potionName;
+      return this.potions;
+
     // Change code above this line
   },
 };
-atTheOldToad.removePotion('Dragon breath')
+atTheOldToad.updatePotionName("Dragon breath", "Polymorth");
+atTheOldToad.updatePotionName("Stone skin", "Invisibility");
